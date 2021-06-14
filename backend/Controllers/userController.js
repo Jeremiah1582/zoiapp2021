20098ec759d867b4f21698903d6d23bab99a147a
jeremiah1582 <jeremiah.1582@googlemail.com>
2021-06-14 15:34:41 +0200

    setup router,controller folders and auth folders

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,17 @@
+
+//********** login function***********
+module.exports= login=()=>{
+    //assign cookie to the 
+    
+}
+
+// ****************** signup function************
+module.exports= signup=()=>{
+    //Create user in database 
+
+}
+
+//****************** logout function***************/
+module.export=logout=()=>{
+    //delete the cookies here
+}
