894b4dc29315965e7a9b12e209e712d768eb64f5
ashikkhandani <mdashikahammed@gmail.com>
2021-06-14 15:28:09 +0200

    userModel and dataBase setup done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const PORT= process.env.PORT
+// import
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const session = require("express-session");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use('/')
-app.use('/user')
+// Settings
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+// express-session
+app.use(
+  session({
+    secret: process.env.MY_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 
+// Database Settings
+const DB_NAME = process.env.DB_NAME;
+const DB_LINK = process.env.MONGO_LINK + DB_NAME;
+mongoose
+  .connect(DB_LINK, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("Mongoose found his way to the database...");
+  })
+  .catch((err) => {
+    console.log(err, "Mongoose lost his connection!");
+  });
 
-app.listen(PORT, ()=>{
-    console.log('you are logged into port '+ PORT );
-})
+// Routes
+app.use("/");
+app.use("/user");
+
+// Listening PORT
+app.listen(PORT, () => {
+  console.log("you are logged into port: " + PORT);
+});
