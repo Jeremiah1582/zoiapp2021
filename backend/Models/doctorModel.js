d8678d91f23849aae56b28372477e92a0b82cc05
ashikkhandani <mdashikahammed@gmail.com>
2021-06-16 14:50:35 +0200

    added models files

---
+++ backend/Models/doctorModel.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const newDoctor = new Schema({
+  accountType: {
+    type: String,
+  },
+  user: {
+    firstName: {
+      type: String,
+      required: [true, "First name is required!"],
+      default: "First name",
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required!"],
+      default: "Last name",
+    },
+    email: {
+      type: String,
+      required: [true, "E-mail is required!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+  },
+  licenseNumber: {
+    type: String,
+    required: [true, "License number is required!"],
+  },
+  yearOfCertificate: {
+    type: String,
+  },
+  specialistIn: {
+    type: String,
+  },
+  languages: {
+    type: String,
+  },
+  address: {
+    street: {
+      type: String,
+    },
+    houseNr: {
+      type: String,
+    },
+    postalCode: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+  },
+});
+
+const Doctor = mongoose.model("Doctor", newDoctor);
+module.exports = Doctor;
