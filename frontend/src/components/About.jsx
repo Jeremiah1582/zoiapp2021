d7e09e1e9466ee859d2c26a81286c073afdba184
jose <info@josecampos.io>
2021-06-30 12:24:13 +0200

    about us page reedited with services section

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,6 +3,9 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import ambulance2 from '../imgs/ambulance-2.jpeg';
 import pharmacy from '../imgs/pharmacy-cross.jpeg';
+import appointment from "../imgs/appointment.jpg";
+import findYourDoctor from "../imgs/findyourdoctor.jpg";
+import aboutUs from "../imgs/aboutus.png";
 
 const About = () => {
   return (
@@ -11,6 +14,7 @@ const About = () => {
       
       < Navbar />
       <div className="aboutSubWrapper">
+        <h2 className = "aboutTitle">About Us</h2>
         <section className="aboutInfo">
           <img src= { ambulance2 } alt="ambulance" className = "ambulanceImg"/>
           <p className = "aboutText">
@@ -23,6 +27,65 @@ const About = () => {
           <p className = "aboutText">
             They floated in the coffin for Armitage’s call. The semiotics of the Sprawl’s towers and ragged Fuller domes, dim figures moving toward him in the dark. The Sprawl was a yearly pilgrimage to Tokyo, where genetic surgeons reset the code of his DNA, a procedure unavailable in Chiba. The two surviving Founders of Zion were old men, old with the movement of the train, their high heels like polished hooves against the gray metal of the console in faded pinks and yellows. Sexless and inhumanly patient, his primary gratification seemed to he in his jacket pocket. The knives seemed to have been sparsely decorated, years before, with a luminous digital display wired to a subcutaneous chip. A narrow wedge of light from a half-open service hatch framed a heap of discarded fiber optics and the chassis of a skyscraper canyon. The color of its skin reminded him of Zone’s whores; it was a handgun and nine rounds of ammunition, and as he made his way down Shiga from the sushi stall he cradled it in his devotion to esoteric forms of tailor-worship. The semiotics of the spherical chamber. He stared at the clinic, Molly took him to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the wall between the bookcases, its distorted face sagging to the bare concrete floor.
           </p>
+        </section>
+        <section>
+          <h2 className = "aboutTitle">Services</h2>
+          <div className="cardWrap">
+            <a href="#">
+              <div className="card">
+                <div className="boxCard">
+                  <div className="contentCard">
+                    <img
+                      src={findYourDoctor}
+                      alt="find a doctor picture"
+                      className="imgCard"
+                    />
+                    <h2>Find a Doctor</h2>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Animi neque quo et provident labore, incidunt maxime alias.
+                      Consequuntur ipsum temporibus mollitia, quae placeat dolorum,
+                      eum odio illum, quos eius nemo?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="/about">
+              <div className="card">
+                <div className="boxCard">
+                  <div className="contentCard">
+                    <img src={aboutUs} alt="about us picture" className="imgCard" />
+                    <h2>About Us</h2>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Animi neque quo et provident labore, incidunt maxime alias.
+                      Consequuntur ipsum temporibus mollitia, quae placeat dolorum,
+                      eum odio illum, quos eius nemo?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="#">
+              <div className="card">
+                <div className="boxCard">
+                  <div className="contentCard">
+                    <img src={appointment} alt="appointment" className="imgCard" />
+                    <h2>Appointments</h2>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Animi neque quo et provident labore, incidunt maxime alias.
+                      Consequuntur ipsum temporibus mollitia, quae placeat dolorum,
+                      eum odio illum, quos eius nemo?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
         </section>
       
       </div>
