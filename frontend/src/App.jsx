48f2c0013d42a6f3733fa80ccc03e7b524ea1205
jeremiah1582 <jeremiah.1582@googlemail.com>
2021-07-06 17:07:15 +0200

    resolving conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,8 @@ import LoginForm from "./components/LoginForm";
 import RegsDoctorForm from "./components/DoctorRegForm";
 import RegsPatientForm from "./components/PatientRegForm";
 import Signup from "./components/Signup";
-<<<<<<< HEAD
-import DashboardPage from "./components/Dashboard/DashboardPage.js";
-import PatientDashboard from "./components/Dashboard/PatientDashboard.js"
-=======
 import DashboardPage from "./components/Dashboard/DashboardPage";
 import PatientDashboard from "./components/Dashboard/PatientDashboard";
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
 
 // import Footer from "./components/Footer";
 import Team from "./components/Team";
@@ -71,29 +66,12 @@ const App = () => {
             <RegsPatientForm />
           </Route>
 
-<<<<<<< HEAD
-          <Route
-            path="/doctor"
-            render={() => {
-              "Doctor Page";
-            }}
-          ></Route>
-          <Route
-            path="/patient/dashboard"> 
-            <PatientDashboard/>
-            </Route>
-          <Route
-            path="/dr/dashboard">
-            <DashboardPage/>
-            </Route>
-=======
           <Route path="/patientdashboard">
             <PatientDashboard />
           </Route>
           <Route path="/doctordashboard">
             <DashboardPage />
           </Route>
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
         </Switch>
       </div>
     </Router>
--- frontend/src/components/Dashboard/PatientDashboard.js
+++ frontend/src/components/Dashboard/PatientDashboard.js
@@ -1,15 +1,9 @@
 import React from "react";
 // import DashNav from './DashNav';
 // import "../../styling/customDashboard.scss";
-<<<<<<< HEAD
-
-//dashNav Widgets:
-import Home from './PatientWidgets/Home'
-=======
 import '../../styling/customDashboard.css'
 //dashNav Widgets:
 import Home from "./PatientWidgets/PDashHome";
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
 // import Calendar from "./PatientWidgets/Calendar";
 // import Messages from "./PatientWidgets/Messages";
 // import PatientList from "./PatientWidgets/PatientList";
@@ -31,11 +25,7 @@ function PatientDashboard() {
             <Link className="dash-menu-item dash-menu-link" to="/home">
               <i className="fas fa-home" />
             </Link>
-<<<<<<< HEAD
-           <Link className="dash-menu-item dash-menu-link" to="/FindDoctor">
-=======
             <Link className="dash-menu-item dash-menu-link" to="/finddoctor">
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
               <i className="fas fa-user-md"></i>
             </Link>
             {/* <Link className="dash-menu-item dash-menu-link" to="/calendar">
@@ -45,21 +35,11 @@ function PatientDashboard() {
             {/* <Link className="dash-menu-item dash-menu-link" to="/messages">
               <i className="fas fa-comments"></i>
             </Link> */}
-<<<<<<< HEAD
-{/* 
-            <Link className="dash-menu-item dash-menu-link" to="/patientList">
-              <i className="fas fa-user-injured"></i>
-            </Link> */}
-
-            
-
-=======
 
             {/* <Link className="dash-menu-item dash-menu-link" to="/patientList">
               <i className="fas fa-user-injured"></i>
             </Link> */}
 
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
             {/* <Link className="dash-menu-item dash-menu-link" to="/todolist">
               <i className="fas fa-th-list"></i>
             </Link> */}
@@ -67,48 +47,12 @@ function PatientDashboard() {
             {/* <Link className="dash-menu-item dash-menu-link" to="/settings">
               <i className="fas fa-cog"></i>
             </Link> */}
-<<<<<<< HEAD
-            
-=======
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
           </ul>
         </div>
 
         {/* <main className="widget-display">
           display
           </main> */}
-<<<<<<< HEAD
-          <Switch>
-            <Route exact path="/home">
-            <Home/>
-            </Route> 
-            <Route path="/FindDoctor">
-              <FindDoctor />
-            </Route>
-            {/* <Route path="/calendar">
-              <Calendar />
-            </Route>
-            <Route path="/messages">
-              <Messages />
-            </Route>
-            <Route path="/patientList">
-              <PatientList />
-            </Route>
-           
-            <Route path="/todolist">
-              <TodoList />
-            </Route>
-            <Route path="/weather">
-              <Weather />
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route> */}
-          </Switch>
-        
-      
-    </div>
-=======
         <Switch>
           <Route exact path="/home">
             <Home />
@@ -136,7 +80,6 @@ function PatientDashboard() {
           </Route> */}
         </Switch>
       </div>
->>>>>>> 31fd17caeb9b7455f253ffd9cef7091627ac12b2
     </DashRouter>
   );
 }
