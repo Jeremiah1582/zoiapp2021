33c57671b1b204542e3e48929cdfcb600a676382
ashikkhandani <mdashikahammed@gmail.com>
2021-07-27 11:24:08 +0200

    styling patient home dashboard

--- frontend/src/components/Dashboard/PatientWidgets/Home.js
+++ frontend/src/components/Dashboard/PatientWidgets/Home.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 // import image1 from '../../../styling/images/operacion-retorno-mallorca-by-andres-nieto-porras.jpg'
 import { Link } from "react-router-dom";
 import { MyContext } from "../../../Context_APIs/userContextAPI";
+import UserIcon from "../../../imgs/usericon.png";
+import Items from "../../../imgs/items.jpg"
 
 function Home() {
   const { userState, getUser } = useContext(MyContext);
@@ -41,60 +43,73 @@ function Home() {
     // test code ashik
     <div className="cardWrap">
       {/* <a href="/patient/finddoctor"> */}
-        <div className="card">
-          <div className="boxCard">
-            <div className="contentCard">
-              {/* <img
-                src={findYourDoctor}
-                alt="find a doctor"
-                className="imgCard"
-              /> */}
-              <h2>Find a Doctor</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-                neque quo et provident labore, incidunt maxime alias.
-                Consequuntur ipsum temporibus mollitia, quae placeat dolorum,
-                eum odio illum, quos eius nemo?
-              </p>
+      <div className="card">
+        <div className="boxCard">
+          <div className="contentCard user-icon">
+            <img src={UserIcon} alt="user icon" className="imgCard " />
+            <h4 className="user-title">Hello {userState.firstName} </h4>
+            <hr />
+            {/* user infos */}
+            <div className="user-info">
+              <div className="user-info-items">
+                Account-Type : {userState.accountType}
+              </div>
+
+              <div className="user-info-items">
+                First Name : {userState.firstName}
+              </div>
+              <div className="user-info-items">
+                Last Name : {userState.lastName}
+              </div>
+              <div className="user-info-items">
+                Birth Date : {userState.birthDate}
+              </div>
+
+              <div className="user-info-items">
+                Insurance Nmr : {userState.insuranceNumber}
+              </div>
+              <div className="user-info-items">
+                Insurance Company : {userState.insuranceCompany}
+              </div>
+              <div className="user-info-items">E-mail : {userState.email}</div>
+              <div className="user-info-items">
+                Mobile Nmr : {userState.mobileNumber}
+              </div>
             </div>
           </div>
         </div>
+      </div>
       {/* </a> */}
 
       {/* <a href="/aboutzoe"> */}
-        <div className="card">
-          <div className="boxCard">
-            <div className="contentCard">
-              {/* <img 
-              src={aboutUs}
-               alt="about us" className="imgCard" /> */}
-              <h2>About Us</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-                neque quo et provident labore, incidunt maxime alias.
-                Consequuntur ipsum temporibus mollitia, quae placeat dolorum,
-                eum odio illum, quos eius nemo?
-              </p>
-            </div>
+      <div className="card">
+        <div className="boxCard">
+          <div className="contentCard">
+            <img 
+              src={Items}
+               alt="items" className="imgCard" />
+            <h2>What do you need ?</h2>
+          
           </div>
         </div>
+      </div>
       {/* </a> */}
 
       {/* <a href="/user/login"> */}
-        <div className="card">
-          <div className="boxCard">
-            <div className="contentCard">
-              {/* <img src={appointment} alt="appointment" className="imgCard" /> */}
-              <h2>Appointments</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
-                neque quo et provident labore, incidunt maxime alias.
-                Consequuntur ipsum temporibus mollitia, quae placeat dolorum,
-                eum odio illum, quos eius nemo?
-              </p>
-            </div>
+      <div className="card">
+        <div className="boxCard">
+          <div className="contentCard">
+            {/* <img src={appointment} alt="appointment" className="imgCard" /> */}
+            <h2>Appointments</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
+              neque quo et provident labore, incidunt maxime alias. Consequuntur
+              ipsum temporibus mollitia, quae placeat dolorum, eum odio illum,
+              quos eius nemo?
+            </p>
           </div>
         </div>
+      </div>
       {/* </a> */}
     </div>
   );
